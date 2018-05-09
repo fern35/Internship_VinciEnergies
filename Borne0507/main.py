8713f450ff481ac82efa4b6411089af21d98f19e
ZHANGyuan <fern35121321@outlook.com>
2018-05-09 11:15:46 +0200

    Update main.py

--- Borne0507/main.py
+++ Borne0507/main.py
@@ -15,6 +15,8 @@ title_name, df_start1, df_start2, df_stop = processor.retriv_startstop(source_fo
 df_sort = processor.merge_sort(path=result_folder,title_name=title_name,df_lst=[df_start1,df_stop,df_start2])
 
 # then check the excel file and remove the anomalies manually
+correct_file = "Example_sorttemp"
+df_sort_corr = pd.read_excel(result_folder+correct_file+".xlsx")
 
 # use the correct excel file to generate the final result.
 df_result = processor.gen_conso(result_folder,title_name,df_sort)
