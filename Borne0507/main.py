e588daf8a2b92c3bd24be253bdea2e71bff308da
ZHANGyuan <fern35121321@outlook.com>
2018-05-09 11:11:47 +0200

    Update main.py

--- Borne0507/main.py
+++ Borne0507/main.py
@@ -14,7 +14,7 @@ title_name, df_start1, df_start2, df_stop = processor.retriv_startstop(source_fo
 # merge the dataframe which stores the info of startTransaction, stopTransaction. Sort the result
 df_sort = processor.merge_sort(path=result_folder,title_name=title_name,df_lst=[df_start1,df_stop,df_start2])
 
-# then check the excel file and remove the anomalies
+# then check the excel file and remove the anomalies manually
 
 # use the correct excel file to generate the final result.
 df_result = processor.gen_conso(result_folder,title_name,df_sort)
